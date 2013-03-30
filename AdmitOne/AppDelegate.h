d5ddb736aa2166c4cf040972087ecf3436fdd29b
aplourde <anthony.plourde@gmail.com>
2013-03-30 08:26:22 -0400

    other license headers

--- AdmitOne/AppDelegate.h
+++ AdmitOne/AppDelegate.h
@@ -3,7 +3,20 @@
 //  AdmitOne
 //
 //  Created by Anthony Plourde on 11-12-22.
-//  Copyright (c) 2011 Anthony Plourde. All rights reserved.
+//  Copyright (c) 2012 Anthony Plourde.
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <Cocoa/Cocoa.h>
@@ -17,15 +30,15 @@
 @class APPreferencesWindowController;
 @class APMovie;
 
-@interface AppDelegate : NSObject <NSApplicationDelegate, SCEventListenerProtocol>{
-    
+@interface AppDelegate : NSObject <NSApplicationDelegate, SCEventListenerProtocol> {
+
     APMainViewController *_mainViewController;
     APMovieDetailsViewController *_detailsViewController;
     APDownloadViewController *_downloadViewController;
     APPreferencesWindowController *_preferencesWindowController;
-    
+
     GAJavaScriptTracker *_tracker;
-    
+
     IBOutlet NSButton *_topRentalsSectionButton;
     IBOutlet NSButton *_currentReleasesSectionButton;
     IBOutlet NSButton *_newReleasesSectionButton;
@@ -35,18 +48,26 @@
     IBOutlet NSSearchField *_searchField;
 }
 
-@property (assign) IBOutlet NSWindow *window;
+@property(assign) IBOutlet NSWindow *window;
 
--(void)showMovieDetails:(APMovie*)movie;
--(void)refreshApplicationBadgeLabel;
--(void)loadDefaultSettings;
+- (void)showMovieDetails:(APMovie *)movie;
 
--(IBAction)showPreferences:(id)sender;
--(IBAction)back:(id)sender;
--(IBAction)showTopRentals:(id)sender;
--(IBAction)showNewReleases:(id)sender;
--(IBAction)showCurrentReleases:(id)sender;
--(IBAction)showDownloads:(id)sender;
--(IBAction)search:(id)sender;
+- (void)refreshApplicationBadgeLabel;
+
+- (void)loadDefaultSettings;
+
+- (IBAction)showPreferences:(id)sender;
+
+- (IBAction)back:(id)sender;
+
+- (IBAction)showTopRentals:(id)sender;
+
+- (IBAction)showNewReleases:(id)sender;
+
+- (IBAction)showCurrentReleases:(id)sender;
+
+- (IBAction)showDownloads:(id)sender;
+
+- (IBAction)search:(id)sender;
 
 @end
